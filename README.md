3730fb9f6a899f7bc5c40b770b5ef6166818694a
valgolei <84638017+valgolei@users.noreply.github.com>
2021-06-05 10:52:19 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ README.md
@@ -0,0 +1,33 @@
+ 
+
+
+> Ouvrir cette page à [https://valgolei.github.io/zelda/](https://valgolei.github.io/zelda/)
+
+## Utiliser comme extension
+
+Ce dépôt peut être ajouté en tant qu'**extension** dans MakeCode.
+
+* ouvrir [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* cliquez sur **Nouveau projet**
+* cliquez sur **Extensions** dans le menu engrenage
+* recherchez **https://github.com/valgolei/zelda** et importez
+
+## Éditer ce projet ![Badge du statut de la compilation](https://github.com/valgolei/zelda/workflows/MakeCode/badge.svg)
+
+Éditer ce dépôt dans MakeCode.
+
+* ouvrir [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* cliquez sur **Importer** puis cliquez sur **Importer l'URL **
+* collez **https://github.com/valgolei/zelda** et cliquez sur importer
+
+## Aperçu des blocs
+
+Cette section montre le code des blocs du dernier commit dans la branche master.
+Cette image peut prendre quelques minutes pour être actualisée.
+
+![Un rendu de la vue des blocs](https://github.com/valgolei/zelda/raw/master/.github/makecode/blocks.png)
+
+#### Métadonnées (utilisées pour la recherche, le rendu)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,47 @@
+namespace SpriteKind {
+    export const cofre = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.cofre, function (sprite, otherSprite) {
+    mySprite = sprites.create(img`
+        . . b b b b b b b b b b b b . . 
+        . b e 4 4 4 4 4 4 4 4 4 4 e b . 
+        b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+        b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+        b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+        b e e 4 4 4 4 4 4 4 4 4 4 e e b 
+        b e e e e e e e e e e e e e e b 
+        b e e e e e e e e e e e e e e b 
+        b b b b b b b d d b b b b b b b 
+        c b b b b b b c c b b b b b b c 
+        c c c c c c b c c b c c c c c c 
+        b e e e e e c b b c e e e e e b 
+        b e e e e e e e e e e e e e e b 
+        b c e e e e e e e e e e e e c b 
+        b b b b b b b b b b b b b b b b 
+        . b b . . . . . . . . . . b b . 
+        `, SpriteKind.cofre)
+})
+let mySprite: Sprite = null
+let link = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(link, 100, 100)
+scene.setBackgroundColor(7)
+tiles.setTilemap(tilemap`niveau1`)
+tiles.placeOnRandomTile(link, sprites.dungeon.stairNorth)
+scene.cameraFollowSprite(link)
